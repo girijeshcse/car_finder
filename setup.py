d6254569253234e9e955188a61b60d596ddc8c19
Girijesh Prasad <girijeshcse@gmail.com>
2021-11-27 08:33:26 +0530

    first commit in car finder

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Capstone project based on CV and Object detection',
+    author='GL Jan21 B',
+    license='MIT',
+)
